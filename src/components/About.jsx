a9e64cc692e2e39635c831722c2fddf197c9241e
Pavanz09 <130452135+Pavanz09@users.noreply.github.com>
2025-05-28 18:02:27 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,11 +14,11 @@ const About = () => {
     <div className='myIntroduction'>
     <p style="text-align: justify; line-height: 27px;">
     Hello and welcome! I’m <span style="color: rgb(255, 0, 79); font-weight: bolder;">Pavan Satyappanavar</span>, a dedicated Full Stack Web Developer with a strong background in software engineering from Jain College of Engineering. My professional journey has equipped me with hands-on experience in modern web development, UI/UX design, and frontend-backend integration. In addition to coding, I actively work on creative projects involving photo and video editing, ensuring a well-rounded approach to visual presentation.
-    <br><br>
+    <br>
     I'm passionate about delivering scalable, high-performance digital solutions that are both visually appealing and functionally robust. My experience spans across React, Node.js, AWS, cloud deployment, automation scripting with Shell, and mobile development with React Native.
-    <br><br>
+    <br>
     Outside of work, I draw creative inspiration from PC gaming and travel. Both experiences help me approach challenges with a fresh perspective and inject a sense of interactivity and exploration into my projects.
-    <br><br>
+    <br>
     Thank you for taking the time to explore my portfolio. I’m excited about the opportunity to collaborate and contribute to meaningful digital experiences.
     </p>
     </div>
